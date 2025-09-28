26e1f19b160f85af456b9736a68d8baa0ee7cc95
wakar-malik <wakar.tech@gmail.com>
2025-09-28 20:35:49 +0530

    create email handler class to send welcome and reset-password email, build email templates, sending welcome email as-soon-as user sign-up, using brevo for sending real emails

--- utils/email.js
+++ utils/email.js
@@ -1,30 +1,74 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
 
-const sendEmail = (options) => {
-  // 1 create transporter
-  const transporter = nodemailer.createTransport({
-    // if we are using wel-known services like gmail/yahoo etc, we don't need to specify the host and port, just use service with name and nodemailer will automatically setup the host and port
-    // with gmail we can only send 500 mails per day
-    // service: "Gmail",
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Wakar Malik <${process.env.EMAIL_FROM}>`;
+  }
 
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return nodemailer.createTransport({
+        // if we are using wel-known services like gmail/yahoo etc, we don't need to specify the host and port, just use service with name and nodemailer will automatically setup the host and port
+        // with gmail we can only send 500 mails per day
+        // service: "Gmail",
 
-  // 2 define options
-  const emailOptions = {
-    from: "Wakar Malik <wakarmalik9@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+        host: process.env.BRAVO_HOST,
+        port: process.env.BRAVO_PORT,
+        auth: {
+          user: process.env.BRAVO_USERNAME,
+          pass: process.env.BRAVO_PASSWORD,
+        },
+      });
+    }
 
-  // 3 send email
-  return transporter.sendMail(emailOptions);
+    return nodemailer.createTransport({
+      // if we are using wel-known services like gmail/yahoo etc, we don't need to specify the host and port, just use service with name and nodemailer will automatically setup the host and port
+      // with gmail we can only send 500 mails per day
+      // service: "Gmail",
+
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // create html template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    //define email options
+    const emailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html),
+    };
+
+    // create transporter and send email
+    await this.newTransport().sendMail(emailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the node-tours");
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
+  }
 };
-
-module.exports = sendEmail;
