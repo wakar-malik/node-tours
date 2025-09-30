e81f3dc6aa7437c5e1baca33459f88df77a37303
wakar-malik <wakar.tech@gmail.com>
2025-10-01 03:32:32 +0530

    add product image for stripe checkout page

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,6 +25,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get("host")}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
           unit_amount: tour.price,
         },
@@ -75,8 +80,6 @@ exports.webHookCheckout = async (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-
-  console.log(event);
 
   if (event.type === "checkout.session.completed") {
     createBookingCheckout(event.data.object);
