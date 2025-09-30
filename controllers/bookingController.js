e502ed9781aa5cd63d3dcd602508edc369e72566
wakar-malik <wakar.tech@gmail.com>
2025-10-01 02:01:07 +0530

    fix createBookingCheckout function

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,7 +57,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 async function createBookingCheckout(session) {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount;
+  const price = session.data[0].price_data.unit_amount;
+
+  console.log(tour, user, price);
 
   await Booking.create({ tour, user, price });
 }
